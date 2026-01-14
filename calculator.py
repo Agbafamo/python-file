aef6d8a179d6324687e3551bcf1bd4b1a5c0c6e1
Oluwafemi Shorunke <shorunkeoluwafemi@gmail.com>
2026-01-14 15:21:06 +0100

    created a calculator.py file

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("""
+**************
+ADDING MACHINE 
+**************
+""")
+
+sum_one = int(input("Enter the first number:"))
+sum_two = int(input("Enter the second number:"))
+
+sum_total = sum_one + sum_two
+output = "Total =" + str(sum_total)
+
+print(output)
--- timestamp.py
+++ timestamp.py
@@ -1,1 +1,6 @@
-print("snapshot files .exe")
+# A SIMPLE TEXT-BASED CALCULATOR.
+
+name = input("What is your name?")
+hello_user = "Hello," + name
+# Greet the User
+print(hello_user) 
